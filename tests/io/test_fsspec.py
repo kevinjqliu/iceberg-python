cd3d9a9b1672e6f221100fd7a2389cd9c9e26aea
Kevin Liu <kevin.jq.liu@gmail.com>
2025-06-19 08:58:12 -0700

    raise AssertionError

--- tests/io/test_fsspec.py
+++ tests/io/test_fsspec.py
@@ -293,13 +293,13 @@ def test_fsspec_unified_session_properties() -> None:
 @pytest.mark.adls
 def test_fsspec_new_input_file_adls(adls_fsspec_fileio: FsspecFileIO) -> None:
     """Test creating a new input file from an fsspec file-io"""
-    assert False, "This should fail CI"
     filename = str(uuid.uuid4())
 
     input_file = adls_fsspec_fileio.new_input(f"abfss://tests/{filename}")
 
     assert isinstance(input_file, fsspec.FsspecInputFile)
     assert input_file.location == f"abfss://tests/{filename}"
+    raise AssertionError
 
 
 @pytest.mark.adls
